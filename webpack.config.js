d573acb555e74a97fb4744928f6df0ceca132bc6
robertogriel <roberto@griel.com.br>
2021-03-30 19:10:13 -0300

    First commit with login

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require("path")
+
+module.exports = {
+    mode: 'development',
+    entry: './public/assets/js/index.js',
+    output: {
+        filename: 'javascript.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    devtool: 'eval-source-map'
+}
