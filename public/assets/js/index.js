d00f4788209bd5112e2c242ff9c09f5fb4f01ffe
robertogriel <roberto@griel.com.br>
2021-04-10 11:45:16 -0300

    Undo

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -2,5 +2,14 @@ import './firebase';
 
 import './Class/ClassLogin';
 import './Class/ClassUtils';
+import './Class/ClassMenu';
+import './Class/ClassSection';
+import './Class/ClassFirebase';
 
 import './Functions/FunctionLogin';
+import './Functions/FunctionMenu.js';
+import './Functions/FunctionSection';
+import './Functions/FunctionsFirebase';
+
+///
+
