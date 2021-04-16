6a5cd2c4f256c45a513fd5463ea7407753f0b94f
robertogriel <roberto@griel.com.br>
2021-04-16 14:44:08 -0300

    PWA fix

--- public/worker.js
+++ public/worker.js
@@ -7,13 +7,10 @@ const urlsToCache = [
 ]
 
 self.addEventListener('install', function(event){
-    if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') {
-        return;
-      }
     event.waitUntil(
         caches.open('v1')
         .then(function(cache){
-            return cachevent.addAll(urlsToCache)
+            return cache.addAll(urlsToCache)
         })
     );
 });
@@ -23,9 +20,6 @@ self.addEventListener('activate', event => {
 })
 
 self.addEventListener('fetch', event => {
-    if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') {
-        return;
-      }
     event.respondWith(
         caches.match(event.request).then( response => {
             return response || fetch(event.request)
